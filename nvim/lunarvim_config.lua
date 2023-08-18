8d1c693d00890ec2c6fdfb25df0f89f97fcdff41
Marek Rehora <mrehora@medallia.com>
2023-08-18 11:57:35 +0200

    Create lunarvim_config.lua
    Add lunarvim config

---
+++ nvim/lunarvim_config.lua
@@ -0,0 +1,123 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+vim.opt.relativenumber = true -- relative line numbers
+vim.opt.wrap = true           -- wrap lines
+vim.opt.scrolloff = 10        -- keep the cursor of the border
+vim.opt.autoindent = true     -- automatically add indent
+
+-- autocomannd to open hover on focus
+
+lvim.format_on_save.enabled = true
+lvim.builtin.dap.active = true
+
+lvim.keys.normal_mode["grn"] = vim.lsp.buf.rename
+
+lvim.plugins = {
+  {
+    "scalameta/nvim-metals",
+    config = function()
+      require("user.metals").config()
+    end,
+  },
+  {
+    "romgrk/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup {
+        enable = true,   -- Enable this plugin (Can be enabled/disabled later via commands)
+        throttle = true, -- Throttles plugin updates (may improve performance)
+        max_lines = 0,   -- How many lines the window should span. Values <= 0 mean no limit.
+        patterns = {     -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+          -- For all filetypes
+          -- Note that setting an entry here replaces all other patterns for this entry.
+          -- By setting the 'default' entry below, you can control which nodes you want to
+          -- appear in the context window.
+          default = {
+            'class',
+            'function',
+            'method',
+          },
+        },
+      }
+    end
+  },
+  -- {
+  --   'codota/tabnine-nvim',
+  --   build = "./dl_binaries.sh",
+  --   config = function()
+  --     require("tabnine").setup({
+  --       disable_auto_comment = true,
+  --       accept_keymap = "<C-Enter>",
+  --       dismiss_keymap = "<C-]>",
+  --       debounce_ms = 800,
+  --       suggestion_color = { gui = "#808080", cterm = 244 },
+  --       exclude_filetypes = { "TelescopePrompt" },
+  --       log_file_path = nil, -- absolute path to Tabnine log file
+  --     })
+  --     require('tabnine.status').status()
+  --   end
+  -- },
+  -- {
+  --   "Pocco81/auto-save.nvim",
+  --   config = function()
+  --     require("auto-save").setup()
+  --   end,
+  -- },
+  {
+    "github/copilot.vim",
+    event = "VeryLazy",
+    config = function()
+      -- copilot assume mapped
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_no_tab_map = true
+    end,
+  },
+  {
+    "hrsh7th/cmp-copilot",
+    config = function()
+      lvim.builtin.cmp.formatting.source_names["copilot"] = "( )"
+      table.insert(lvim.builtin.cmp.sources, 2, { name = "copilot" })
+    end,
+  },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   event = "InsertEnter",
+  --   dependencies = { "zbirenbaum/copilot.lua" },
+  --   config = function()
+  --     vim.defer_fn(function()
+  --       require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+  --       require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+  --     end, 100)
+  --   end,
+  -- },
+  {
+  "f-person/git-blame.nvim",
+  event = "BufRead",
+  config = function()
+    vim.cmd "highlight default link gitblame SpecialComment"
+    vim.g.gitblame_enabled = 0
+  end,
+},
+}
+
+lvim.builtin.dap.on_config_done = function(dap)
+  dap.configurations.scala = {
+    {
+      type = "scala",
+      request = "launch",
+      name = "Run or Test Target",
+      metals = {
+        runType = "runOrTestFile",
+      },
+    },
+    {
+      type = "scala",
+      request = "launch",
+      name = "Test Target",
+      metals = {
+        runType = "testTarget",
+      },
+    },
+  }
+end
