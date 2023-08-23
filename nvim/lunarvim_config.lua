b7d1e3d89b0eca24a55f66d10568f47a1c73660a
Marek Rehora <mrehora@medallia.com>
2023-08-23 15:18:25 +0200

    Update lunarvim_config.lua

--- nvim/lunarvim_config.lua
+++ nvim/lunarvim_config.lua
@@ -7,12 +7,42 @@ vim.opt.wrap = true           -- wrap lines
 vim.opt.scrolloff = 10        -- keep the cursor of the border
 vim.opt.autoindent = true     -- automatically add indent
 
--- autocomannd to open hover on focus
+vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = "LspAttach_inlayhints",
+  callback = function(args)
+    if not (args.data and args.data.client_id) then
+      return
+    end
+
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    require("lsp-inlayhints").on_attach(client, bufnr)
+  end,
+})
 
 lvim.format_on_save.enabled = true
 lvim.builtin.dap.active = true
 
 lvim.keys.normal_mode["grn"] = vim.lsp.buf.rename
+
+-- Trouble keybinds
+lvim.keys.normal_mode["<leader>xx"] = function() require("trouble").open() end
+lvim.keys.normal_mode["<leader>xw"] = function() require("trouble").open("workspace_diagnostics") end
+lvim.keys.normal_mode["<leader>xd"] = function() require("trouble").open("document_diagnostics") end
+lvim.keys.normal_mode["<leader>xq"] = function() require("trouble").open("quickfix") end
+lvim.keys.normal_mode["<leader>xl"] = function() require("trouble").open("loclist") end
+lvim.keys.normal_mode["<leader>xr"] = function() require("trouble").open("lsp_references") end
+
+--TODO lvim.builtin.which_key.mappings["x"] = {
+--   name = "Trouble",
+--   r = { "<cmd>Trouble lsp_references<cr>", "References" },
+--   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+--   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+--   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+--   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+--   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+-- }
 
 lvim.plugins = {
   {
@@ -20,6 +50,10 @@ lvim.plugins = {
     config = function()
       require("user.metals").config()
     end,
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
   },
   {
     "romgrk/nvim-treesitter-context",
@@ -37,6 +71,9 @@ lvim.plugins = {
             'class',
             'function',
             'method',
+            'object',
+            'case',
+            'for'
           },
         },
       }
@@ -64,41 +101,45 @@ lvim.plugins = {
   --     require("auto-save").setup()
   --   end,
   -- },
+  -- {
+  -- "hrsh7th/cmp-copilot",
+  -- config = function()
+  -- lvim.builtin.cmp.formatting.source_names["copilot"] = "( )"
+  -- table.insert(lvim.builtin.cmp.sources, 2, { name = "copilot" })
+  -- end,
+  -- },
   {
-    "github/copilot.vim",
-    event = "VeryLazy",
+    "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
+    dependencies = { "zbirenbaum/copilot.lua" },
     config = function()
-      -- copilot assume mapped
-      vim.g.copilot_assume_mapped = true
-      vim.g.copilot_no_tab_map = true
-    end,
-  },
-  {
-    "hrsh7th/cmp-copilot",
-    config = function()
-      lvim.builtin.cmp.formatting.source_names["copilot"] = "( )"
-      table.insert(lvim.builtin.cmp.sources, 2, { name = "copilot" })
+      vim.defer_fn(function()
+        require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+        require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+      end, 100)
     end,
   },
   -- {
-  --   "zbirenbaum/copilot-cmp",
+  --   "zbirenbaum/copilot.lua",
   --   event = "InsertEnter",
-  --   dependencies = { "zbirenbaum/copilot.lua" },
   --   config = function()
-  --     vim.defer_fn(function()
-  --       require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-  --       require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
-  --     end, 100)
+  --     require("copilot").setup({})
+  --   end,
+  -- },
+  -- {
+  --   "f-person/git-blame.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     vim.cmd "highlight default link gitblame SpecialComment"
+  --     vim.g.gitblame_enabled = 0
   --   end,
   -- },
   {
-  "f-person/git-blame.nvim",
-  event = "BufRead",
-  config = function()
-    vim.cmd "highlight default link gitblame SpecialComment"
-    vim.g.gitblame_enabled = 0
-  end,
-},
+    "lvimuser/lsp-inlayhints.nvim",
+    config = function()
+      require("lsp-inlayhints").setup()
+    end,
+  }
 }
 
 lvim.builtin.dap.on_config_done = function(dap)
